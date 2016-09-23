f1685628d3b804c5b9713be06007d56be2f494d5
Erik Älgekrans <erik.algekrans@assaabloy.com>
2016-09-23 14:30:14 +0200

    Added a Cocoa Touch Framework target Updated project to the "recommended settings"

---
+++ LoopBackCarthage/LoopBackCarthage.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoopBackCarthage.
+FOUNDATION_EXPORT double LoopBackCarthageVersionNumber;
+
+//! Project version string for LoopBackCarthage.
+FOUNDATION_EXPORT const unsigned char LoopBackCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoopBackCarthage/PublicHeader.h>
+
+
